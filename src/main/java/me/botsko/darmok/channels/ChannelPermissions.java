a5c6582593cbd66455372ca1ff94248c500d575f
Mike Botsko <botsko@gmail.com>
2013-03-14 00:09:35 -0700

    Restoring banned channel perms

--- src/main/java/me/botsko/darmok/channels/ChannelPermissions.java
+++ src/main/java/me/botsko/darmok/channels/ChannelPermissions.java
@@ -1,5 +1,7 @@
 package me.botsko.darmok.channels;
 
+import me.botsko.darmok.Darmok;
+
 import org.bukkit.entity.Player;
 
 public class ChannelPermissions {
@@ -44,9 +46,9 @@ public class ChannelPermissions {
 	public static boolean playerCanDefaultTo( Player player, Channel channel ){
 		String permPrefix = "darmok.channel." + channel.getName().toLowerCase() + ".";
 		if( player.hasPermission( permPrefix + "default" ) || player.hasPermission( permPrefix + "read" ) || player.hasPermission( permPrefix + "speak" ) ){
-//			if( ! Settings.isPlayerBannedFromChannel(player, channel) ){ @todo fix this
+			if( ! Darmok.getPlayerRegistry().isPlayerBannedFromChannel(player, channel) ){
 				return true;
-//			}
+			}
 		}
 		return false;
 	}
@@ -61,9 +63,9 @@ public class ChannelPermissions {
 	public static boolean playerCanJoin( Player player, Channel channel ){
 		String permPrefix = "darmok.channel." + channel.getName().toLowerCase() + ".";
 		if( player.hasPermission( permPrefix + "read" ) || player.hasPermission( permPrefix + "speak" ) ){
-//			if( ! Settings.isPlayerBannedFromChannel(player, channel) ){ @todo fix this
+			if( ! Darmok.getPlayerRegistry().isPlayerBannedFromChannel(player, channel) ){
 				return true;
-//			}
+			}
 		}
 		return false;
 	}
@@ -123,7 +125,9 @@ public class ChannelPermissions {
 	public static boolean playerCanSpeak( Player player, Channel channel ){
 		String permPrefix = "darmok.channel." + channel.getName().toLowerCase() + ".";
 		if( player.hasPermission( permPrefix + "speak" ) ){
-			return true;
+			if( ! Darmok.getPlayerRegistry().isPlayerBannedFromChannel(player, channel) ){
+				return true;
+			}
 		}
 		return false;
 	}
